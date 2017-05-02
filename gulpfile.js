d3530caa2d5af227bcf941acd109dfe2527892ba
jakebillings <jake@jakebillings.com>
2017-05-01 22:30:03 -0600

    Add google analytics

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,14 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var runSequence = require('run-sequence');
+var inject = require('gulp-inject-string');
+var gulpIf = require('gulp-if');
+
+var ANALYTICS = '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)};i[r].l=1*new Date();a=s.createElement(o);m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');\nga(\'create\', \'UA-98392131-1\', \'auto\');ga(\'send\', \'pageview\');</script>';
 
 gulp.task('build', function () {
     return gulp.src('./app/**/*')
+        .pipe(gulpIf(/.*\.html/,inject.after('<!-- Analytics -->', ANALYTICS)))
         .pipe(gulp.dest('./dist'));
 });
 
